67fe9a6564cbe94e0dff73566dacb5bbf9e26d1e
yoonminsang <apdlyooapdl@naver.com>
2022-01-27 17:41:33 +0900

    chore: 공통타입추가

--- src/types/game.ts
+++ src/types/game.ts
@@ -9,3 +9,12 @@ export interface IGameInitOption extends ILocation {
 
 export type TGraph = ('mine' | number | null)[][];
 export type TCurrentGraph = ('notSelect' | 'mine' | number | null | 'question')[][];
+export type TDifficulty = 'beginner' | 'intermediate' | 'expert' | 'custom';
+
+export interface IDifficulty {
+  difficulty: TDifficulty;
+  mine?: number;
+  row?: number;
+  column?: number;
+}
+// TODO: notSelect 변수명 수정?
