25fa3b7a7f72a10b2b99aace7156d96f003e5b75
yoonminsang <apdlyooapdl@naver.com>
2022-01-28 06:33:51 +0900

    chore: 불필요한 파일 제거

--- src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-  id: string;
-  email: string;
-  nickname: string;
-}
-
-export interface ILogUser extends IUser {
-  accessToken: string;
-}
--- src/types/error.ts
+++
@@ -1,3 +0,0 @@
-export interface IError {
-  errorMessage: string;
-}
