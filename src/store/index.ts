de3d334370ca51cce1f30ece3cb72fd830f9e5cf
yoonminsang <apdlyooapdl@naver.com>
2022-01-27 17:41:53 +0900

    feat: store에 메뉴 연결

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,18 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import { gameReducer, gameSaga } from './game';
+import { menuReducer, menuSaga } from './menu';
 
 const rootReducer = combineReducers({
   game: gameReducer,
+  menu: menuReducer,
 });
 
 type RootState = ReturnType<typeof rootReducer>;
 
 function* rootSaga(): Generator {
   try {
-    yield all([gameSaga()]);
+    yield all([gameSaga(), menuSaga()]);
   } catch (err) {
     console.log(err);
   }
