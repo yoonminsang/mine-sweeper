005e38e828fc9547de67aa015cd29dd8ae5f8cf9
yoonminsang <apdlyooapdl@naver.com>
2022-01-28 03:24:47 +0900

    feat: 왼쪽 클릭하는 경우 사가함수 로직추가

--- src/saga/game.ts
+++ src/saga/game.ts
@@ -17,6 +17,52 @@ function* leftClickSaga(action: PayloadAction<ILocation>): Generator {
     game: { graph, currentGraph },
   } = (yield select()) as RootState;
   const { row, column } = action.payload;
+  const graphCell = graph[row][column];
+  const nextCurrentGraph = currentGraph.map((v) => v.slice());
+  if (nextCurrentGraph[row][column] !== 'notSelect') return;
+  switch (graphCell) {
+    case 'mine':
+      for (let i = 0; i < graph.length; i++) {
+        for (let j = 0; j < graph[0].length; j++) {
+          if (typeof nextCurrentGraph[i][j] === 'number') continue;
+          if (nextCurrentGraph[i][j] === 'flag') {
+            if (graph[i][j] !== 'mine') {
+              nextCurrentGraph[i][j] = 'bombmIsFlagged';
+              continue;
+            }
+          } else if (graph[i][j] === 'mine') {
+            nextCurrentGraph[i][j] = 'bombRevealed';
+            continue;
+          }
+        }
+      }
+      nextCurrentGraph[row][column] = 'bombDeath';
+      yield put({ type: gameStore.failGame.type });
+      break;
+    case 0: {
+      const graphRow = graph.length;
+      const graphColumn = graph[0].length;
+      const visit = Array(graphRow)
+        .fill(null)
+        .map(() => Array(graphColumn).fill(false));
+      const fn = (row: number, column: number) => {
+        // 동서남북
+        if (visit[row][column]) return;
+        visit[row][column] = true;
+        if (typeof graph[row][column] === 'number') nextCurrentGraph[row][column] = graph[row][column] as number;
+        if (graph[row][column] !== 0) return;
+        if (column + 1 < graphColumn) fn(row, column + 1);
+        if (column - 1 >= 0) fn(row, column - 1);
+        if (row - 1 >= 0) fn(row - 1, column);
+        if (row + 1 < graphRow) fn(row + 1, column);
+      };
+      fn(row, column);
+      break;
+    }
+    default:
+      nextCurrentGraph[row][column] = graphCell;
+  }
+  yield put({ type: gameStore.clickSuccess.type, payload: nextCurrentGraph });
 }
 
 function* rightClickSaga(action: PayloadAction<ILocation>): Generator {
