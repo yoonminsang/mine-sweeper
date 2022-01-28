ed391c261503f3de492b590964cfe52a5f0a6e1d
yoonminsang <apdlyooapdl@naver.com>
2022-01-28 18:53:29 +0900

    refactor: 함수 분리

--- src/saga/game.ts
+++ src/saga/game.ts
@@ -2,8 +2,46 @@ import { PayloadAction } from '@reduxjs/toolkit';
 import { put, select } from 'redux-saga/effects';
 import * as gameStore from '@/store/game';
 import { RootState } from '@/store';
-import { ILocation } from '@/types/game';
+import { ILocation, TCurrentGraph, TGraph } from '@/types/game';
 import { makeBasicGraph, makeGraph } from '@/utils';
+
+const pressMine = (graph: TGraph, nextCurrentGraph: TCurrentGraph, row: number, column: number) => {
+  for (let i = 0; i < graph.length; i++) {
+    for (let j = 0; j < graph[0].length; j++) {
+      if (typeof nextCurrentGraph[i][j] === 'number') continue;
+      if (nextCurrentGraph[i][j] === 'flag') {
+        if (graph[i][j] !== 'mine') {
+          nextCurrentGraph[i][j] = 'bombmIsFlagged';
+          continue;
+        }
+      } else if (graph[i][j] === 'mine') {
+        nextCurrentGraph[i][j] = 'bombRevealed';
+        continue;
+      }
+    }
+  }
+  nextCurrentGraph[row][column] = 'bombDeath';
+};
+
+const pressEmpty = (graph: TGraph, nextCurrentGraph: TCurrentGraph, row: number, column: number) => {
+  const graphRow = graph.length;
+  const graphColumn = graph[0].length;
+  const visit = Array(graphRow)
+    .fill(null)
+    .map(() => Array(graphColumn).fill(false));
+  const changeGraph = (row: number, column: number) => {
+    if (visit[row][column]) return;
+    visit[row][column] = true;
+    if (typeof graph[row][column] === 'number') nextCurrentGraph[row][column] = graph[row][column] as number;
+    if (graph[row][column] !== 0) return;
+    // 동서남북
+    if (column + 1 < graphColumn) changeGraph(row, column + 1);
+    if (column - 1 >= 0) changeGraph(row, column - 1);
+    if (row - 1 >= 0) changeGraph(row - 1, column);
+    if (row + 1 < graphRow) changeGraph(row + 1, column);
+  };
+  changeGraph(row, column);
+};
 
 function* initGraphSaga(): Generator {
   const {
@@ -18,49 +56,22 @@ function* leftClickSaga(action: PayloadAction<ILocation>): Generator {
   const {
     game: { graph, currentGraph, isProcess, isEnd },
   } = (yield select()) as RootState;
+
   if (isEnd) return;
   if (!isProcess) yield put({ type: gameStore.startGame.type });
+
   const { row, column } = action.payload;
-  const graphCell = graph[row][column];
-  const nextCurrentGraph = currentGraph.map((v) => v.slice());
+  const nextCurrentGraph = currentGraph.map((v) => v.slice()); // 2차원 배열 복사
   if (nextCurrentGraph[row][column] !== 'notSelect') return;
+
+  const graphCell = graph[row][column];
   switch (graphCell) {
     case 'mine':
-      for (let i = 0; i < graph.length; i++) {
-        for (let j = 0; j < graph[0].length; j++) {
-          if (typeof nextCurrentGraph[i][j] === 'number') continue;
-          if (nextCurrentGraph[i][j] === 'flag') {
-            if (graph[i][j] !== 'mine') {
-              nextCurrentGraph[i][j] = 'bombmIsFlagged';
-              continue;
-            }
-          } else if (graph[i][j] === 'mine') {
-            nextCurrentGraph[i][j] = 'bombRevealed';
-            continue;
-          }
-        }
-      }
-      nextCurrentGraph[row][column] = 'bombDeath';
+      pressMine(graph, nextCurrentGraph, row, column);
       yield put({ type: gameStore.failGame.type });
       break;
     case 0: {
-      const graphRow = graph.length;
-      const graphColumn = graph[0].length;
-      const visit = Array(graphRow)
-        .fill(null)
-        .map(() => Array(graphColumn).fill(false));
-      const fn = (row: number, column: number) => {
-        // 동서남북
-        if (visit[row][column]) return;
-        visit[row][column] = true;
-        if (typeof graph[row][column] === 'number') nextCurrentGraph[row][column] = graph[row][column] as number;
-        if (graph[row][column] !== 0) return;
-        if (column + 1 < graphColumn) fn(row, column + 1);
-        if (column - 1 >= 0) fn(row, column - 1);
-        if (row - 1 >= 0) fn(row - 1, column);
-        if (row + 1 < graphRow) fn(row + 1, column);
-      };
-      fn(row, column);
+      pressEmpty(graph, nextCurrentGraph, row, column);
       break;
     }
     default:
