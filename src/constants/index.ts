ac85c50da99ca3e0b18060be9d4c7331c34b90bf
yoonminsang <apdlyooapdl@naver.com>
2022-01-29 23:59:31 +0900

    fix: 난이도 expert 행, 열 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const DIFFICULTY = {
   beginner: { mine: 10, row: 8, column: 8 },
   intermediate: { mine: 40, row: 16, column: 16 },
-  expert: { mine: 99, row: 32, column: 16 },
+  expert: { mine: 99, row: 16, column: 32 },
 };
