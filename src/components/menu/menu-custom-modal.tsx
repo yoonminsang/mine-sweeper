2ca31ba91c53d141d6c3b6dcf5801af1ef6379b8
yoonminsang <apdlyooapdl@naver.com>
2022-01-30 00:29:53 +0900

    wip: 메뉴 커스텀 모달 컴포넌트 스타일링

--- src/components/menu/menu-custom-modal.tsx
+++ src/components/menu/menu-custom-modal.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled, { css } from 'styled-components';
 import useInputs from '@/hooks/use-inputs';
 import { IGameInitOption } from '@/types/game';
@@ -12,7 +12,7 @@ interface IProps {
 }
 
 const Wrapper = styled.form<{ customVisible: boolean }>`
-  width: 100px;
+  width: 130px;
   position: absolute;
   background-color: #c0c0c0;
   border: 1px solid black;
@@ -27,13 +27,38 @@ const Wrapper = styled.form<{ customVisible: boolean }>`
     `};
 `;
 
-const Row = styled.div``;
+const BorderDiv = styled.div`
+  border: 1px solid black;
+  margin: 1px;
+`;
 
-const Label = styled.label``;
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 3px;
+`;
 
-const Input = styled.input``;
+const Label = styled.div`
+  width: 70px;
+`;
 
-const Button = styled.button``;
+const Input = styled.input`
+  width: 50px;
+  background-color: lightgray;
+  padding-left: 3px;
+`;
+
+const ButtonRow = styled(Row)`
+  justify-content: space-around;
+`;
+
+const Button = styled.button`
+  border: 1px solid black;
+  border-radius: 5px;
+  width: 40px;
+  font-weight: bold;
+  background-color: darkgray;
+`;
 
 const MenuCustomModal: React.FC<IProps> = ({
   customVisible,
@@ -42,26 +67,32 @@ const MenuCustomModal: React.FC<IProps> = ({
   gameInitOption,
 }) => {
   const [{ row, column, mine }, onChange] = useInputs({ ...gameInitOption });
+  const rowRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (customVisible && rowRef.current) rowRef.current.focus();
+  }, [customVisible]);
   return (
     <Wrapper customVisible={customVisible} onSubmit={(e) => onSubmitCustomDifficultyHandler(e, { row, column, mine })}>
-      <Row>
-        <Label>행</Label>
-        <Input type="text" value={row} onChange={onChange} name="row" />
-      </Row>
-      <Row>
-        <Label>열</Label>
-        <Input type="text" value={column} onChange={onChange} name="column" />
-      </Row>
-      <Row>
-        <Label>지뢰 숫자</Label>
-        <Input type="text" value={mine} onChange={onChange} name="mine" />
-      </Row>
-      <Row>
-        <Button type="submit">확인</Button>
-        <Button type="button" onClick={onCancelCustomDifficultyHandler}>
-          취소
-        </Button>
-      </Row>
+      <BorderDiv>
+        <Row>
+          <Label>행</Label>
+          <Input type="text" value={row} onChange={onChange} name="row" ref={rowRef} />
+        </Row>
+        <Row>
+          <Label>열</Label>
+          <Input type="text" value={column} onChange={onChange} name="column" />
+        </Row>
+        <Row>
+          <Label>지뢰 숫자</Label>
+          <Input type="text" value={mine} onChange={onChange} name="mine" />
+        </Row>
+        <ButtonRow>
+          <Button type="submit">확인</Button>
+          <Button type="button" onClick={onCancelCustomDifficultyHandler}>
+            취소
+          </Button>
+        </ButtonRow>
+      </BorderDiv>
     </Wrapper>
   );
 };
