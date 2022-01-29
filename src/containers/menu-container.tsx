cec28dba8921ffeca5923f4d5b13e415fa8631e2
yoonminsang <apdlyooapdl@naver.com>
2022-01-30 00:03:34 +0900

    feat: menu container 생성

--- src/containers/menu-container.tsx
+++ src/containers/menu-container.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Menu from '@/components/menu';
+import { RootState } from '@/store';
+import { changeDifficulty } from '@/store/menu';
+import { IDifficulty } from '@/types/game';
 
 const MenuContainer: React.FC = () => {
-  return <Menu />;
+  const dispatch = useDispatch();
+  const { difficulty, gameInitOption } = useSelector(({ menu }: RootState) => ({
+    difficulty: menu.difficulty,
+    gameInitOption: { mine: menu.mine, row: menu.row, column: menu.column },
+  }));
+
+  const onChangeDifficulty = useCallback(
+    (difficultyObj: IDifficulty) => {
+      dispatch({ type: changeDifficulty.type, payload: difficultyObj });
+    },
+    [dispatch],
+  );
+  return <Menu difficulty={difficulty} onChangeDifficulty={onChangeDifficulty} gameInitOption={gameInitOption} />;
 };
 
 export default MenuContainer;
